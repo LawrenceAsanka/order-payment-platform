e6ffcfb6f34e4b061cfacf27428aef27f79a86d0
asanka <asankalorance@gmail.com>
2026-08-19 22:14:42 +0200

    Add health endpoint and controller test

---
+++ services/order-service/src/test/java/lk/asanka/orders/controller/OrderControllerTest.java
@@ -0,0 +1,27 @@
+package lk.asanka.orders.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void health_shouldReturnUp() throws Exception {
+
+        mockMvc.perform(
+                        get("/api/v1/orders/health")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+}
