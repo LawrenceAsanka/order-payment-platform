821679aaf731c3f4c9bd980409a18eb27bfa633b
asanka <asankalorance@gmail.com>
2026-08-23 16:39:50 +0200

    Config RestClient Bean

--- services/order-service/src/main/java/lk/asanka/orders/client/ProductServiceClient.java
+++ services/order-service/src/main/java/lk/asanka/orders/client/ProductServiceClient.java
@@ -1,19 +1,20 @@
 package lk.asanka.orders.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.util.UUID;
 
 @Component
-public class ProductServiceClient implements ProductClient{
+public class ProductServiceClient implements ProductClient {
 
-    private final RestClient restClient;
+    private final RestClient productServiceRestClient;
 
-
-    public ProductServiceClient(RestClient restClient) {
-        this.restClient = restClient;
+    public ProductServiceClient(RestClient.Builder restClientBuilder,
+                                @Value("${product-service.base-url}") String productServiceBaseUrl) {
+        productServiceRestClient = restClientBuilder.baseUrl(productServiceBaseUrl)
+                .build();
     }
 
     @Override
---
+++ services/order-service/src/main/java/lk/asanka/orders/config/RestClientConfig.java
@@ -0,0 +1,14 @@
+package lk.asanka.orders.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient.Builder restClient(){
+        return RestClient.builder();
+    }
+}
